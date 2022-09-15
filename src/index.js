0f7b0b4b2759c5b3ca76b2dc90ce06205f61a63c
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-09-15 23:28:24 +0300

    ........

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-import './css/styles.css';
+import { galleryItems } from './gallery-items';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
+
+
+
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+};
+
+
+
+
+let galleryItem = '';
+const buildGallery = galleryItems.map(({ original, preview, description }) => {
+    galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+        </div>`;
+});
+
+ref.containerForGallery.innerHTML = galleryItem;
+
+
+new SimpleLightbox('.gallery__link', {
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
+console.log(galleryItems);
+
+
+const { height: cardHeight } = document
+    .querySelector(".gallery")
+    .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+});
